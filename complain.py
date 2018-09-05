62247c9502de1c859fd5a32704f0a341ad5a4bd1
Babu Thomas <babuthomas138@gmail.com>
2018-09-05 12:25:56 +0530

    Login to Excitel website

--- complain.py
+++ complain.py
@@ -1,5 +1,17 @@
 from selenium import webdriver
 
+username = ''
+password = ''
+
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-driver.close()
+driver.get('https://my.excitel.com/login')
+username_element = driver.find_element_by_xpath('//form//input[@formcontrolname="username"]')
+username_element.clear()
+username_element.send_keys(username)
+password_element = driver.find_element_by_xpath('//form//input[@formcontrolname="password"]')
+password_element.clear()
+password_element.send_keys(password)
+login_button_element = driver.find_element_by_xpath('//form//button')
+login_button_element.click()
+
+# driver.close()
