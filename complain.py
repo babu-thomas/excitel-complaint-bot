54da8f0feb677bb2f887bc86e17b2ce96fc5b7d6
Babu Thomas <babuthomas138@gmail.com>
2018-09-05 12:31:39 +0530

    Refactor login code into a function

--- complain.py
+++ complain.py
@@ -1,17 +1,21 @@
 from selenium import webdriver
 
-username = ''
-password = ''
 
-driver = webdriver.Chrome()
-driver.get('https://my.excitel.com/login')
-username_element = driver.find_element_by_xpath('//form//input[@formcontrolname="username"]')
-username_element.clear()
-username_element.send_keys(username)
-password_element = driver.find_element_by_xpath('//form//input[@formcontrolname="password"]')
-password_element.clear()
-password_element.send_keys(password)
-login_button_element = driver.find_element_by_xpath('//form//button')
-login_button_element.click()
+def login(driver, username, password):
+    driver.get('https://my.excitel.com/login')
+    username_element = driver.find_element_by_xpath('//form//input[@formcontrolname="username"]')
+    username_element.clear()
+    username_element.send_keys(username)
+    password_element = driver.find_element_by_xpath('//form//input[@formcontrolname="password"]')
+    password_element.clear()
+    password_element.send_keys(password)
+    login_button_element = driver.find_element_by_xpath('//form//button')
+    login_button_element.click()
 
-# driver.close()
+
+if __name__ == '__main__':
+    username = ''
+    password = ''
+    driver = webdriver.Chrome()
+    login(driver, username, password)
+    # driver.close()
